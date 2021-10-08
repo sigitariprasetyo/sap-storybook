87a9d0ae76788511e478df0ec724b347da9ed845
sigitariprasetyo <sigitari111@gmail.com>
2021-10-08 16:02:50 +0700

    renaming story

--- src/components/Input/Input.stories.js
+++ src/components/Input/Input.stories.js
@@ -9,3 +9,6 @@ export default {
 export const Small = () => <Input size="small" placeholder="Small" />
 export const Medium = () => <Input size="medium" placeholder="Medium" />
 export const Large = () => <Input size="large" placeholder="Large" />
+
+//Renaming Story
+Small.storyName = "Small Input"
